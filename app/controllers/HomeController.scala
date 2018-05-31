bb0b25d1f8505dbc8a0832149c0117ef0546f2ce
steve delp <steve.delp@gmail.com>
2018-05-31 13:38:40 -0600

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+import play.api.mvc._
+import play.twirl.api.Html
+
+import scala.util.{Failure, Success, Try}
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+    def index() = Action {
+        Ok("Hello, World!")
+    }
+
+  }
---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "ScalaPlayApp"
+
+version := "1.0.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.12.2"
+
+libraryDependencies += guice
